ff4308a8a8560f490f7766fddea2fb4a5944136c
Ayush Goyal <ayush.goyal@winzogames.com>
2026-01-09 23:50:09 +0530

    Initial Commit

---
+++ SwiggyAssignment/SwiggyAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiggyAssignmentApp.swift
+//  SwiggyAssignment
+//
+//  Created by Ayush Goyal on 09/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiggyAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
